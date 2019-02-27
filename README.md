7fb91c0f2c2869e677359614be142f871944f618
JJ <jenjones@ucdavis.edu>
2019-02-26 16:33:11 -0800

    concert, spotify, and readme updates

--- README.md
+++ README.md
@@ -3,17 +3,43 @@
 ## Description
 LIRI is a Language Interpretation and Recognition Interface. This is a command line node app that takes in arguements entered by the user and returns data.
 
-## User Interface
-
-
 ## Installs
 * Axios `npm install axios`
 * Moment.js `npm install moment`
 * Spotify `npm install --save node-spotify-api`
 
+## User Interface
+Enter `node liri.js` + one of these four commands:
+1. `concert-this` + `<artist/band name here>` displays the following:
+    * Venue name
+    * Venue location
+    * Event date
+
+2. `spotify-this-song` + `<song name here>` displays the following:
+    * Artist name
+    * Song name
+    * Song preview URL from Spotify
+    * Album name
+    * NOTE:  If no song name is entered, LIRI defaults to "The Sign" by Ace of Base
+
+3. `movie-this` + `<movie name here>` displays the following:
+    * Title
+    * Year
+    * IMDB Rating
+    * Rotten Tomatoes Rating
+    * Country where the movie was produced
+    * Language
+    * Plot
+    * Actors
+    * NOTE:  If no movie name is entered, LIRI defaults to "Mr. Nobody"
+
+4. `do-what-it-says` 
+    * Uses the fs Node package to take the text inside the random.txt file to call one of LIRI's commands.
+
 ## Technology
+* API - OMDB (via Axios)
+* API - Bands in Town (via Axios)
 * Node.js
+* NPM Spotify
 * JavaScript
-* OMDB API (via Axios)
-* Bands in Town API (via Axios)
-* Spotify API
+* Moment.js
--- liri.js
+++ liri.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 var fs = require("fs");
 var axios = require("axios");
-var moment = require('moment');
+var moment = require("moment");
 var keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
 // Command line arguements entered by user
-var command = process.argv[2];
-var query = process.argv[3];
+const command = process.argv[2];
+const query = process.argv.slice(3);
 
 if (command === "movie-this") {
     movie();
@@ -26,18 +26,57 @@ if (command === "movie-this") {
     console.log("Invalid command. Please enter a different command.")
 }
 
+// ----------Bands in Town API using Axios----------
+// Function to respond to concert-this command
+function concert() {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + query.join('+') + "/events?app_id=codingbootcamp";
+
+    console.log(queryUrl);
+    axios.get(queryUrl).then(
+        function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                // console.log(response.data);
+                // console.log(Object.keys(response.data));           
+                console.log("* Venue name: " + response.data[i].venue.name);
+                console.log("* Venue location: " + response.data[i].venue.city + "," + response.data[i].venue.region);
+                console.log("* Event Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+                console.log("\r\n\=================================\r\n");
+            }
+        },
+        function (error) {
+            console.log(error);
+        });
+}
+
+// ----------Spotify API using npm package----------
+// Function to respond to spotify-this-song command
+function song() {
+    spotify.search({ type: 'track', query: query }, function (error, data) {
+        var songs = data.tracks.items;
+
+        if (error) {
+            console.log('Error occurred: ' + error);
+        } else {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                console.log("Artist name: " + songs[i].artists[0].name);
+                console.log("Song name: " + songs[i].name);
+                console.log("Preview song URL: " + songs[i].preview_url);
+                console.log("Album name: " + songs[i].album.name);
+                console.log("\r\n\=================================\r\n");
+            }
+        }
+    });
+}
+
+// ----------OMDB API---------- 
+// Function to respond to movie-this command
 function movie() {
-    // OMDB API
-    var queryUrl = "http://www.omdbapi.com/?t=" + query + "&apikey=trilogy";
-
-    // if (process.argv.length > 3) {
-    //     process.argv[].join("+");
-    // };
-
-    // Default to the movie Twister if User fails to input movie title
-    if (query === undefined) {
-        queryUrl = "http://www.omdbapi.com/?t=twister&apikey=trilogy";
+    // Default movie if User fails to input movie title
+    if (query === "") {
+        query = "Mr. Nobody";
     };
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + query + "&apikey=trilogy";
 
     axios.get(queryUrl).then(
         function (response) {
@@ -50,42 +89,18 @@ function movie() {
             console.log("* Language of the movie: " + response.data.Language);
             console.log("* Plot of the movie: " + response.data.Plot);
             console.log("* Actors in the movie: " + response.data.Actors);
+            console.log("\r\n\=================================\r\n");
         },
         function (error) {
             console.log(error);
         });
 }
 
-function song() {
-    spotify.search({ type: 'track', query: query }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-
-        console.log(data);
-    });
-}
-
-function concert() {
-    // Bands in Town API
-    var queryUrl = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
-
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log(response.data);
-            // console.log("* Venue name: " + response.data.venue[4]);
-            // console.log("* Venue location: " + response.data.venue[1]);
-            console.log("* Event Date: " + moment(response.datetime).format("MM/DD/YYYY"));
-        },
-        function (error) {
-            console.log(error);
-        });
-}
-
+// Function to respond to do-what-it-says command
 function doIt() {
-    fs.readFile("random.txt","utf8",function(err,data) {
-        if(err) {
-            return console.log(err);
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
         }
-});
+    });
 }
